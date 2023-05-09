f4d20b28d2b92063e0cd3f91b7b3ca9bf3d6cda8
lshoek <lesleyvanhoek@hotmail.com>
2023-05-09 23:17:06 +0200

    port to nap 0.6 - test napfft

--- module/src/mod_graphictool.cpp
+++
@@ -1,3 +0,0 @@
-#include <utility/module.h>
-
-NAP_SERVICE_MODULE("mod_graphictool", "0.1.0", "nap::GraphicToolService")
---
+++ module/src/napgraphictool.cpp
@@ -0,0 +1,3 @@
+#include <utility/module.h>
+
+NAP_SERVICE_MODULE("napgraphictool", "0.1.0", "nap::GraphicToolService")
